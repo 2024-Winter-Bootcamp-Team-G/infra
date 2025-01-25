5a2726853df4be5644e332e346a82109ff1b8c49
jongmine <jongmine.ch@gmail.com>
2025-01-25 16:30:44 +0000

    feat: HTTPS 적용 완료

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,11 +7,11 @@ fi
 
 compose_file="docker-compose.yml"
 
-domains=($DOMAINS)
+domains=("algorify.net")
 rsa_key_size=4096
 data_path="./certbot"
-email=$CERTBOT_EMAIL # Adding a valid address is strongly recommended
-staging=$CERTBOT_STAGING # Set to 1 if you're testing your setup to avoid hitting request limits
+email="jongmine.ch@gmail.com" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
